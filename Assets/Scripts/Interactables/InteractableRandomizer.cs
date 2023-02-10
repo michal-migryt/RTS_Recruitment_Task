b8c61efaa19e69eb6d96008a746455e136e7c147
michal-migryt <michamigryt@gmail.com>
2023-02-10 15:17:52 +0100

    Little bit of bugfixing.
    Prevented infinite loop when too much doors are meant to be spawned. Fixed chest repositioning in wall issue.

--- Assets/Scripts/Interactables/InteractableRandomizer.cs
+++ Assets/Scripts/Interactables/InteractableRandomizer.cs
@@ -20,7 +20,6 @@ public class InteractableRandomizer : MonoBehaviour
         for(int i=0; i< doorAmount; i++)
         {
             GameObject newDoor = Instantiate(doorPrefab, Vector3.zero, doorPrefab.transform.rotation);
-            Debug.Log(newDoor.transform.rotation);
             doorList.Add(newDoor.GetComponent<Door>());
         }
     }
@@ -37,19 +36,28 @@ public class InteractableRandomizer : MonoBehaviour
     }
     private Vector3 RandomPosition(Vector3 objectMeasures)
     {
-        float x = Random.Range(xMin + objectMeasures.x, xMax - objectMeasures.x);
-        float z = Random.Range(zMin + objectMeasures.z, zMax - objectMeasures.z);
+        // to prevent positioning object with tiny bit of it in wall
+        float minimalOffset = 0.05f;
+        float x = Random.Range(xMin + objectMeasures.x + minimalOffset, xMax - objectMeasures.x - minimalOffset);
+        float z = Random.Range(zMin + objectMeasures.z + minimalOffset, zMax - objectMeasures.z - minimalOffset);
         return new Vector3(x, 0, z);
     }
+    private float RandomAngle()
+    {
+        float angle = Random.Range(1, 361);
+        return angle;
+    }
+
     private void RandomizeDoor(Door door)
     {
         bool foundValidPosition = false;
-        
+        door.gameObject.SetActive(true);
         // depth is y component because of twisted door's prefab rotation or something
         float doorWidth = door.GetMeasurements().x, doorDepth = door.GetMeasurements().y/1.5f, doorLength = door.GetMeasurements().z;
-        Debug.Log(doorDepth);
+        int maxTries = 20, tries=0;
         while(!foundValidPosition)
         {
+            tries++;
             int wallNumber = Random.Range(0, 4);
             float x, z;
             Wall chosenWall = (Wall) wallNumber;
@@ -95,14 +103,16 @@ public class InteractableRandomizer : MonoBehaviour
                 door.transform.Rotate(0, 0, wallNumber*90, Space.Self);
                 placedDoors.Add(door);
             }
+            // prevents infinite loop when there is too much doors
+            else if(tries == maxTries)
+            {
+                door.gameObject.SetActive(false);
+                break;
+            }
+            
         }
         
 
     }
-    private float RandomAngle()
-    {
-        float angle = Random.Range(1, 361);
-        return angle;
-    }
-
+    
 }
